acffecaf4f18953c87a7b8f8634d55a1bda2a09c
XiangdongDu <18896588320@163.com>
2021-12-31 11:11:43 +0800

    activi

---
+++ Spring-activiti-spring-boot-activiti/src/main/java/boot/spring/Application.java
@@ -0,0 +1,17 @@
+package boot.spring;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@MapperScan("boot.spring.mapper")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
